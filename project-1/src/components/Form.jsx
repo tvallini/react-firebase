e5fa78263350390351d3476f6af67cdd8e32e565
Tomi Vallini <tomivallini@gmail.com>
2021-06-06 00:04:39 -0300

    simple form

--- project-1/src/components/Form.jsx
+++ project-1/src/components/Form.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+
   return (
     <div className="container">
       <h2>Form</h2>
       <form>
         <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          <label for="name" className="form-label">Name</label>
+          <input 
+            className="form-control" 
+            id="name" 
+            onChange={ (e) => { setName(e.target.value) }}
+            type="text" 
+          />
         </div>
         <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" for="exampleCheck1">Check me out</label>
+          <label for="age" className="form-label">Age</label>
+          <input 
+            className="form-control" 
+            id="age"
+            onChange={ (e) => { setAge(e.target.value) }}
+            type="text" 
+          />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
