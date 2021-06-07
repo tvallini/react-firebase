aa5f30954064730954011d7068b006e8e6f3de68
Tomi Vallini <tomivallini@gmail.com>
2021-06-07 18:16:45 -0300

    add react-router-dom to project-1

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,3 +1,6 @@
+import {
+  BrowserRouter as Router, Switch, Route, Link
+} from 'react-router-dom';
 import './App.css';
 import Counter from './components/Counter';
 import List from './components/List';
