ce738797b40080026254d1c6dfd4270bc2f98f28
Tomi Vallini <tomivallini@gmail.com>
2021-06-09 23:36:57 -0300

    logout ok

--- project-firebase/src/components/Nav.jsx
+++ project-firebase/src/components/Nav.jsx
@@ -1,28 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../firebaseconfig';
 
 const Nav = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged ( (user) => {
+      if (user) {
+        setUser(user.email)
+      }
+    })      
+  }, [])
+
+  const Logout = () => {
+    auth.signOut()
+    setUser(null)
+  }
+
   return (
-    <>  
-      <nav className="navbar navbar-expand navbar-dark bg-primary">
-        <div className="container-fluid">
-          <Link to="/" className="navbar-brand">Project Firebase</Link>
-          <div>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link to="/" className="nav-link">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/admin" className="nav-link">Admin</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/login" className="nav-link">Login</Link>
-              </li>
-            </ul>
-          </div>
+    <nav className="navbar navbar-expand navbar-dark bg-primary">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">Project Firebase</Link>
+        <div>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/admin" className="nav-link">Admin</Link>
+            </li>            
+            {
+              user ?
+              (
+                <li className="nav-item">
+                  <Link 
+                    className="nav-link"
+                    style={{backgroundColor: "#dc3545", borderRadius: "0.25rem"}}
+                    onClick={Logout}
+                  >
+                    Logout
+                  </Link>
+                </li>
+              ):(
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link">Login</Link>
+                </li>
+              )
+            }
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   )
 }
 
