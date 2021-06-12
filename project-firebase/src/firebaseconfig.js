2b5f6b6d3ebb90384847d5596e236a72989ddb10
Tomi Vallini <tomivallini@gmail.com>
2021-06-12 19:48:55 -0300

    Update firebaseconfig.js

--- project-firebase/src/firebaseconfig.js
+++ project-firebase/src/firebaseconfig.js
@@ -3,13 +3,7 @@
   import 'firebase/firestore';
 
   const firebaseConfig = {
-    apiKey: "AIzaSyDeDiGPH_Yr9-SUSosG4t3WCSEVckVLw_M",
-    authDomain: "react-project-95cec.firebaseapp.com",
-    projectId: "react-project-95cec",
-    storageBucket: "react-project-95cec.appspot.com",
-    messagingSenderId: "20588306844",
-    appId: "1:20588306844:web:7234a7c7034c24b1692859",
-    measurementId: "G-01ZF5E4267"
+    // put your data here
   };
   // Initialize Firebase
   const fire = firebase.initializeApp(firebaseConfig);
